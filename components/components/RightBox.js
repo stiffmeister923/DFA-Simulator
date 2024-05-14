3b1430df8f5181e6cf75179ff57521e8163af776
stiffmeister923 <106596734+stiffmeister923@users.noreply.github.com>
2024-05-14 18:36:13 +0800

    Update RightBox.js

--- components/components/RightBox.js
+++ components/components/RightBox.js
@@ -5,8 +5,9 @@ import { CgChevronRight } from "react-icons/cg";
 import FirstDFA from "../DFA/FirstDFA";
 import SecondDFA from "../DFA/SecondDFA";
 import CFG from "./CFG";
-
+import PDA from "./PDA";
 const RightBox = ({
+  pda,
   prob2,
   simulating,
   regex1,
@@ -55,6 +56,7 @@ const RightBox = ({
           </Heading>
           <Flex>
             <CFG prob2={prob2} />
+            <PDA pda={pda} />
             <Button
               disabled={simulating}
               onClick={handleSwitch}
