a7902c48e51997ccd199136ebf9c22c3a72bfa1e
stiffmeister923 <106596734+stiffmeister923@users.noreply.github.com>
2024-05-18 22:14:41 +0800

    Update CFG.js

--- components/components/CFG.js
+++ components/components/CFG.js
@@ -28,7 +28,8 @@ const CFG = ({ prob2 }) => {
         <PopoverContent
           maxW={["17em", null, "16em", null, null, "16em"]}
           _focus=""
-          bg="gray.50"
+          bg="blue.500"
+          color="white"
           fontSize={["0.7em", "0.7em", "0.9em"]}
         >
           <PopoverArrow />
