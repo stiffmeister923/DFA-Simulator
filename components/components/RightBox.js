49c538f2a9e48dce85084d1da6c0aea777f09fc1
stiffmeister923 <106596734+stiffmeister923@users.noreply.github.com>
2024-05-22 22:34:24 +0800

    Update RightBox.js

--- components/components/RightBox.js
+++ components/components/RightBox.js
@@ -7,7 +7,6 @@ import SecondDFA from "../DFA/SecondDFA";
 import CFG from "./CFG";
 import PDA from "./PDA";
 const RightBox = ({
-  pda,
   prob2,
   simulating,
   regex1,
@@ -53,7 +52,7 @@ const RightBox = ({
           </Heading>
           <Flex>
             <CFG prob2={prob2} />
-            <PDA pda={pda} />
+            <PDA pda={prob2} />
             <Button
               disabled={simulating}
               onClick={handleSwitch}
