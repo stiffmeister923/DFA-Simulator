5eba799f0c1423d6bff416e69d1da8f89e4e9b07
stiffmeister923 <106596734+stiffmeister923@users.noreply.github.com>
2024-05-23 17:32:32 +0800

    Update CFG.js

--- components/components/CFG.js
+++ components/components/CFG.js
@@ -43,22 +43,22 @@ const CFG = ({ prob2 }) => {
                <VStack>
                 <Text color="white">{"S → XYZABC"}</Text>
                 <Text color="white">{"X → b | aa | ab"}</Text>
-                <Text color="white">{"Y → aY | bY | ɛ"}</Text>
-                <Text color="white">{"Z → bbZ | abaZ | abZ | ɛ"}</Text>
+                <Text color="white">{"Y → aY | bY | λ"}</Text>
+                <Text color="white">{"Z → bbZ | abaZ | abZ | λ"}</Text>
                 <Text color="white">{"A → aaa | bbb"}</Text>
                 <Text color="white">{"B → a | b"}</Text>
-                <Text color="white">{"C → aC | bC | abC | ɛ"}</Text>
+                <Text color="white">{"C → aC | bC | abC | λ"}</Text>
               </VStack>
             ) : (
               <VStack>
                 <Text color="white">{"S → XYZABCD"}</Text>
-                <Text color="white">{"X → 1X | 0X | ɛ"}</Text>
+                <Text color="white">{"X → 1X | 0X | λ"}</Text>
                 <Text color="white">{"Y → 11 | 00 | 101 | 010"}</Text>
-                <Text color="white">{"Z → 1Z | 0Z | 11Z | 00Z | 101Z | ɛ"}</Text>
+                <Text color="white">{"Z → 1Z | 0Z | 11Z | 00Z | 101Z | λ"}</Text>
                 <Text color="white">{"A → 11 | 00"}</Text>
-                <Text color="white">{"B → 11B | 00B | 101B | ɛ"}</Text>
+                <Text color="white">{"B → 11B | 00B | 101B | λ"}</Text>
                 <Text color="white">{"C → 1 | 0"}</Text>
-                <Text color="white">{"D → 1D | 0D | 11D | ɛ"}</Text>
+                <Text color="white">{"D → 1D | 0D | 11D | λ"}</Text>
               </VStack>
             )}
           </PopoverBody>
